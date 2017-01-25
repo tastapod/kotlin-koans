6de6ad3bd9cfdeccccf4384d5a902351c5bc0710
Dan North <dan@dannorth.net>
2017-01-25 23:02:26 +0000

    make a start on convention koans

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,12 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return this.comparableDate() - other.comparableDate()
+    }
+
+    private fun comparableDate() = this.year * 10000 + this.month * 100 + this.dayOfMonth
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
